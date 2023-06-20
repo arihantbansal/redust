4f0480121774ac2e042c16ba657ed6920adf819a
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-06-20 12:48:07 +0530

    add multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,8 +10,17 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let res = format_response("PONG");
-                _stream.write(res.as_bytes());
+                let mut buf = [0; 512];
+                loop {
+                    let bytes_read = _stream.read(&mut buf).unwrap();
+                    if bytes_read == 0 {
+                        println!("client closed the connection");
+                        break;
+                    }
+
+                    let res = format_response("PONG");
+                    _stream.write(res.as_bytes()).unwrap();
+                }
             }
             Err(e) => {
                 println!("Error: {e}");
